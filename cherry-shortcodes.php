4e3c0b52d412660126c3b09abdd16d4c5bf6614d
cheh <cheh@MacBook-Pro-cheh.local>
2016-06-27 18:05:57 +0300

    Version bump to 1.0.7.5-beta

--- cherry-shortcodes.php
+++ cherry-shortcodes.php
@@ -3,7 +3,7 @@
  * Plugin Name: Cherry Shortcodes
  * Plugin URI:  http://www.cherryframework.com/
  * Description: A pack of WordPress shortcodes.
- * Version:     1.0.7.4
+ * Version:     1.0.7.5-beta
  * Author:      Cherry Team
  * Author URI:  http://www.cherryframework.com/
  * Text Domain: cherry-shortcodes
@@ -83,7 +83,7 @@ if ( !class_exists( 'Cherry_Shortcodes' ) ) {
 			 *
 			 * @since 1.0.0
 			 */
-			define( 'CHERRY_SHORTCODES_VERSION', '1.0.7.4' );
+			define( 'CHERRY_SHORTCODES_VERSION', '1.0.7.5-beta' );
 
 			/**
 			 * Set the slug of the plugin.
